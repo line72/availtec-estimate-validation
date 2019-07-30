6bc3234125700770997f3ed3d170431f4f1a2d47
Marcus Dillavou <line72@line72.net>
2019-07-30 14:57:50 -0500

    Initial version of capturer and grapher

---
+++ config.py
@@ -0,0 +1,28 @@
+URL = 'https://realtimebjcta.availtec.com/InfoPoint'
+
+stops = [
+    {
+        'id': 2492,
+        'description': '10th & 28th Inbound',
+        'direction': 'I',
+        'hit_line': ((33.508138, -86.787964), (33.508757, -86.785763)) # p1(lat, long), p2(lat, long)
+    },
+    {
+        'id': 1431,
+        'description': '20th & 1st S Outbound',
+        'direction': 'O',
+        'hit_line': ((33.511321, -86.804249), (33.511733, -86.803235)) # p1(lat, long), p2(lat, long)
+    },
+    {
+        'id': 2113,
+        'description': '20th & 3rd S Inbound',
+        'direction': 'I',
+        'hit_line': ((33.509628, -86.802964), (33.510005, -86.802213)) # p1(lat, long), p2(lat, long)
+    },
+    {
+        'id': 1437,
+        'description': '20th & 10th S Outbound',
+        'direction': 'O',
+        'hit_line': ((33.501799, -86.797381), (33.501941, -86.797061)) # p1(lat, long), p2(lat, long)
+    },
+]
